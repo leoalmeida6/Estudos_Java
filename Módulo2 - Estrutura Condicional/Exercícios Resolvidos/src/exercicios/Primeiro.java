2ff6778914dc596f21cfcc2e6ef87e582e8ac700
Leonardo Almeida <95828114+leoalmeida6@users.noreply.github.com>
2022-05-19 17:04:38 -0300

    Exercícios Resolvidos - Estrutura Condicional

---
+++ Módulo2 - Estrutura Condicional/Exercícios Resolvidos/src/exercicios/Primeiro.java
@@ -0,0 +1,40 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Primeiro {
+
+    public static void main(String[] args) {
+
+        //Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Variables
+        int numero;
+
+        System.out.println("=============================");
+        System.out.println("    POSITIVO ou NEGATIVO     ");
+        System.out.println("=============================");
+
+        //Pula Linha
+        System.out.println("");
+
+        System.out.printf("Informe um número: ");
+        numero = sc.nextInt();
+
+        if (numero >= 0) {
+            //Pula Linha
+            System.out.println("");
+
+            System.out.printf("O número %d é POSITIVO!", numero);
+        } else {
+            //Pula Linha
+            System.out.println("");
+
+            System.out.printf("O número %d é NEGATIVO!", numero);
+        }
+
+        sc.close();
+    }
+
+}
